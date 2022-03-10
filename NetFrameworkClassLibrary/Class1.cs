1640ebd1b04de9f677975978e1ef5ac2f5842f91
Eliseo Gonzalez <eliseog@herbalife.com>
2022-03-10 14:52:41 -0600

    first commit

---
+++ NetFrameworkClassLibrary/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFrameworkClassLibrary
+{
+    public class Class1
+    {
+        public string Test()
+        {
+            EventLog.WriteEntry("FRANCKYJORGE", "Hello from .net framework");
+            return "HI";
+        }
+    }
+}
